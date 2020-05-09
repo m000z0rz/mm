d81e46cb282588c5aa27bb692f4a0e75de1dcd13
Brian Baker <bbaker47@gatech.edu>
2020-05-08 23:30:32 -0500

    trying again... setup for En_Nnh, and OK

---
+++ src/actors/En_Nnh/ovl_En_Nnh_0x80C08760.c
@@ -0,0 +1,19 @@
+#include <ultra64.h>
+#include <global.h>
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/EnNnh_Init.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/EnNnh_Destroy.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/func_80C08828.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/func_80C0883C.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/func_80C088A4.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/func_80C088B8.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/EnNnh_Update.asm")
+
+GLOBAL_ASM("asm/non_matchings/ovl_En_Nnh_0x80C08760/EnNnh_Draw.asm")
+
